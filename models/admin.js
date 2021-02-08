26fbf61a6d122aa34962ac03309108236bc1f69e
shinigamicoder2000 <shinigamicoder2000@gmail.com>
2021-02-08 19:49:48 +0530

    Add admin model

---
+++ models/admin.js
@@ -0,0 +1,17 @@
+const mongoose = require("../config/mongoose");
+
+const adminSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+});
+
+const Admin = mongoose.model('Admin',adminSchema);
+module.exports = Admin;
