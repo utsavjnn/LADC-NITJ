caed9e63955c740f2e280e682d029cb0585c6986
shinigamicoder2000 <shinigamicoder2000@gmail.com>
2021-02-12 23:31:32 +0530

    Add admin controller and routes

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -1,5 +1,38 @@
-module.exports.home = function(req,res){
-    return res.render('admin',{
-        title:"admin"
-    });
-}
+const Admin = require("../models/admin");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+module.exports.home = (req, res) => {
+  res.render("admin", { title: "admin" });
+};
+module.exports.signUp = function (req, res) {
+  Admin.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err, user);
+        res.redirect("/admin");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/");
+        });
+      }
+    }
+  );
+};
+module.exports.signIn = function (req, res) {
+  const user = new Admin({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/admin");
+      });
+    }
+  });
+};
--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const adminController = require('../controllers/admin_controller');
-
 router.get('/',adminController.home);
+router.post('/sign-in',adminController.signIn);
+router.post('/sign-up',adminController.signUp);
 
 
 
