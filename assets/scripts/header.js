6ac11d848b18a7a9eceb8294a35b13c2b6ef71ff
Harsh Gupta <54658082+harshgupta1512@users.noreply.github.com>
2021-02-17 22:26:20 +0530

    Homepageandevents (#10)
    * Created Homepage and navbar
    
    * Updated the responsiveness of homepage
    
    * Added ladc image and random lorem ipsum text in home page
    
    * Added scripts folder in assets,meta tags in layout file and changed function name
    
    * A minor change in script tag of header.ejs file
    
    * Corrected the Bootstrap Script links, placed them from home.ejs to layout.ejs
    
    * Corrected the position of cdn link for bootstrap
    
    * Adding a little style
    
    Signed-off-by: Utsav Jain <utsavjn@Utsavs-MacBook-Air.local>
    
    * Formatting of files done, removed extra spaces and used beautify vscode extension to increase code readability
    
    * Added LADC logo and updated navbar UI
    
    * Updated UI of homepage, corrected margins of sections
    
    Co-authored-by: Utsav Jain <utsavjn@Utsavs-MacBook-Air.local>

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,16 +1,23 @@
+
 function hamburgerexpandtoggler() 
-    {
-       var element = document.getElementById("mainheading");
-       element.classList.toggle("maintexterase");
-       var element2 = document.getElementById("navbarToggleExternalContent");
-       var ham=document.getElementById("menubutton").getAttribute("aria-expanded");
-       console.log(ham);
-      if(ham==="false")
-      {
-        element2.classList.add("navcolor");
-      }
-       else
-       {
-       element2.classList.remove("navcolor");
-      }
-    }
+{
+  var logoimage=document.getElementById("logoimage");
+   var element = document.getElementById("mainheading");
+   if(window.location.href=="http://localhost:8000/")
+   {
+    element.classList.toggle("maintexterase");
+   }
+   var element2 = document.getElementById("navbarToggleExternalContent");
+   var ham=document.getElementById("menubutton").getAttribute("aria-expanded");
+   console.log(ham);
+  if(ham==="false")
+  {
+    element2.classList.add("navcolor");
+    logoimage.classList.add("logodisappear");
+  }
+   else
+   {
+   element2.classList.remove("navcolor");
+   logoimage.classList.remove("logodisappear");
+  }
+}
