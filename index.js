6d5dee4a79f0914c790167a9d852dee1f915bfa4
dhruvmig <dhruvmiglani.23@gmail.com>
2021-02-19 12:15:12 +0530

    alumni portal
    Signed-off-by: dhruvmig <dhruvmiglani.23@gmail.com>

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 
 const db = require("./config/mongoose");
-const bodyParser=require('body-parser');
+// const bodyParser=require('body-parser');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
