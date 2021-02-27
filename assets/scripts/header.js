27c9c6724c67e16fd7c0c12e6174d5a38a27a027
Harsh Gupta <harshgupta15122000@gmail.com>
2021-02-27 13:56:28 +0530

    Rebasing

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 function hamburgerexpandtoggler() 
 {
@@ -22,21 +22,3 @@ function hamburgerexpandtoggler()
    logoimage.classList.remove("logodisappear");
   }
 }
-=======
-function hamburgerexpandtoggler() 
-    {
-       var element = document.getElementById("mainheading");
-       element.classList.toggle("maintexterase");
-       var element2 = document.getElementById("navbarToggleExternalContent");
-       var ham=document.getElementById("menubutton").getAttribute("aria-expanded");
-       console.log(ham);
-      if(ham==="false")
-      {
-        element2.classList.add("navcolor");
-      }
-       else
-       {
-       element2.classList.remove("navcolor");
-      }
-    }
->>>>>>> 5e37e37 (Added scripts folder in assets,meta tags in layout file and changed function name)
