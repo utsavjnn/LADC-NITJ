b847140b95d9e2ad46f5fffcf9f499bf8ab01502
Harsh Gupta <harshgupta15122000@gmail.com>
2021-02-27 14:03:27 +0530

    Rebasing

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 const db = require("./config/mongoose");
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> cdec7fe (Corrected the header css file)
+=======
+>>>>>>> 59ef3de (Updating changes)
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
