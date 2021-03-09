65c32d587a4ecd5f31af05de1651714742f6ae31
Harsh Gupta <54658082+harshgupta1512@users.noreply.github.com>
2021-03-09 18:54:29 +0530

    Alumnifrontenddesign (#26)
    * Updated a little change in add event logic
    
    * Designed front-end of alumni page
    
    * Updated front-end design of alumni and corrected some other issues in footer front-end design
    
    * navbar color change
    
    Signed-off-by: dhruvmig <dhruvmiglani.23@gmail.com>
    
    Co-authored-by: dhruvmig <dhruvmiglani.23@gmail.com>

--- assets/scripts/alumni.js
+++ assets/scripts/alumni.js
@@ -1,89 +1,91 @@
 function getAlumniList(batch) {
-	$.ajax({
-		url: `http://localhost:8000/alumni/batch/${batch}`,
-		type: 'GET',
-		success: function (alumnis) {
-			console.log(alumnis);
-			document.getElementById('all-alumnis').innerHTML = '';
-			const container = document.getElementById('all-alumnis');
-			alumnis.forEach(alumni => {
-				const card = document.createElement('div');
-				card.classList = 'card-body';
-				// Construct card content
-				let {imageURL} = alumni;
-				if(imageURL !== null && imageURL.length === 0)
-					imageURL = null;
-				let content = `
-          <div class="card col-md-8 col-12">
-          <div class="card-header" id="heading-${alumni._id}">
-            <h5 class="mb-0">
-                  ${alumni.name}
-            </h5>
-          </div>
-          <div class='card-horizontal'>
-              <div class='image-square-wrapper'>
-                <img style='max-width:200px;max-height:200px;' src=${
-					imageURL === null ? '/images/user_default.png' : imageURL
-				} class='card-img-left' alt='User Image'/>
-              </div>
-              <div id="${alumni._id}" class="collapse show" >
-                <div class="card-body">
-                  <h5>${alumni.email}</h5>
-                  <p>${alumni.linkedin}</p>
-                  <p>${alumni.batch}</p>
-                </div>
-              </div>     
-          </div>
-        </div>
-        `;
+  $.ajax({
+    url: `http://localhost:8000/alumni/batch/${batch}`,
+    type: 'GET',
+    success: function (alumnis) {
+      document.getElementById('all-alumnis').innerHTML = '';
+      const container = document.getElementById('all-alumnis');
+      let wholeContent="";
+      wholeContent += '<div class="row">';
+      for(let alumni of alumnis)
+      {
 
-				container.innerHTML += content;
-			});
-		},
-		error: function (err) {
-			console.error('error ', err);
-		},
-	});
+      let content = "";
+        let {
+          imageURL
+        } = alumni;
+        if (imageURL !== null && imageURL.length === 0)
+          imageURL = null;
+        
+        content += `
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+      <div class="our-team">
+        <div class="picture">
+          <img class="img-fluid" src=${
+            imageURL === null ? '/images/user_default.png' : imageURL}>
+        </div>
+        <div class="team-content">
+          <h3 class="name">${alumni.name}</h3>
+          <h5 class="title">${alumni.batch}</h5>
+        </div>
+        <ul class="social">
+          <li><a href="mailto:${alumni.email}" class="fa fa-google-plus" aria-hidden="true"></a></li>
+          <li><a href="${alumni.linkedin}" class="fa fa-linkedin" aria-hidden="true"></a></li>
+        </ul>
+      </div>
+    </div>
+
+        `;
+        wholeContent+=content;
+        
+      };
+      wholeContent += '</div>';
+      container.innerHTML= wholeContent;
+    },
+    error: function (err) {
+      console.error('error ', err);
+    },
+  });
 }
 
 var menu = document.getElementById('batch');
 menu.onchange = function (e) {
-	e.preventDefault();
-	getAlumniList(menu.value);
+  e.preventDefault();
+  getAlumniList(menu.value);
 };
 
 window.onload = function () {
-	var currentBatch = sessionStorage.getItem('batch');
-	console.log('batch is ', currentBatch);
-	if (currentBatch == null) {
-		currentBatch = 'all';
-	}
-	$('#batch').val(currentBatch);
-	getAlumniList(currentBatch);
+  var currentBatch = sessionStorage.getItem('batch');
+  // console.log('batch is ', currentBatch);
+  if (currentBatch == null) {
+    currentBatch = 'all';
+  }
+  $('#batch').val(currentBatch);
+  getAlumniList(currentBatch);
 };
 
 $('#batch').change(function () {
-	var selVal = $(this).val();
-	sessionStorage.setItem('batch', selVal);
+  var selVal = $(this).val();
+  sessionStorage.setItem('batch', selVal);
 });
 
 const hideModal = function (event) {
-	const form = $('.alumni-submit')[0];
-	form.style.transitionDuration = '0ms';
-	form.style.transform = 'scale(0)';
-	let heading = $('.alumni-submit')[0].children[0].children[0];
-	heading.innerHTML = 'Register as an Alumnus';
-	this.style.display = 'none';
+  const form = $('.alumni-submit')[0];
+  form.style.transitionDuration = '0ms';
+  form.style.transform = 'scale(0)';
+  let heading = $('.alumni-submit')[0].children[0].children[0];
+  heading.innerHTML = 'Register as an Alumnus';
+  this.style.display = 'none';
 };
 
 const showModal = event => {
-	const cbutton = $('.cancel-button')[0];
-	cbutton.style.display = 'block';
-	const form = $('.alumni-submit')[0];
-	form.style.transitionDuration = '200ms';
-	form.style.transform = 'scale(1)';
-	if (window.innerWidth <= 450) {
-		let heading = $('.alumni-submit')[0].children[0].children[0];
-		heading.innerHTML = 'Register';
-	}
+  const cbutton = $('.cancel-button')[0];
+  cbutton.style.display = 'block';
+  const form = $('.alumni-submit')[0];
+  form.style.transitionDuration = '200ms';
+  form.style.transform = 'scale(1)';
+  if (window.innerWidth <= 450) {
+    let heading = $('.alumni-submit')[0].children[0].children[0];
+    heading.innerHTML = 'Register';
+  }
 };
