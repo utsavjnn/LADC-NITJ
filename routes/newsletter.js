b60af3b0e2b72b31db2f6ddfa04c0b96c679f100
devcode <adevcodea@gmail.com>
2021-03-13 12:37:43 +0530

    Email Template

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const newsletter_controller = require('../controllers/newsletter_controller');
 
 
-router.post('/subscribe',newsletter_controller.subscribe)
+router.post('/subscribe',newsletter_controller.subscribe);
 
-
+router.post('/unsubscribe',newsletter_controller.un_subscribe);
 
 
 module.exports = router;
