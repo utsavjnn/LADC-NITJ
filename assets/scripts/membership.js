e40ea8c84d793b1896abe14e0491b80a89642969
Dhruv Miglani <dhruvmiglani.23@gmail.com>
2021-03-20 20:34:17 +0530

    membership page

--- assets/scripts/membership.js
+++ assets/scripts/membership.js
@@ -7,7 +7,7 @@ window.onscroll = function(event){
   if(lim <= 100){
     justText.style.opacity = '1';
     otherNav.style.opacity = '0';
-    justText.innerHTML = `<h2>Team LADC</h2>`;
+    justText.innerHTML = `<h2 class="Josephine-Sans">Team LADC</h2>`;
   } else {
     justText.style.opacity = '0';
     otherNav.style.opacity = '1';
@@ -20,50 +20,59 @@ window.onscroll = function(event){
 function getMemberList(batch)
 {
   $.ajax({
-    url:`http://localhost:8000/membership/batch/${batch}`,
+    url:`http://localhost:8000/membership/batch/all`,
     type:"GET",
     success:function(members){
-      document.getElementById("all-members").innerHTML = "";
-      const container = document.getElementById("all-members");
-      members.forEach(member => {
+      // document.getElementById("all-members").innerHTML = "";
+      // const container = document.getElementById("all-members");
+      const container = document.getElementById("final-year");
+      console.log('container is ',container);
+      let finalYear = members.filter(member => member.batch==2021)
+      let content;
+      finalYear.forEach(member => {
         let url = member.image ?? '/images/user_default.png';
         if(typeof member.image !== 'string')
           url = '/images/user_default.png';
         const card = document.createElement('div');
         card.classList = 'card-body';
         // Construct card content
-        let content = `
+         content = `
         <div class='memberCard'>
           <div style='background-image:url("${member.image}")' class='memberImage'>
           </div>
-          <div class='memberDetails'>
+          <div class='memberDetails josephine-sans'>
             <h5>${member.name}</h5>
             <h6>${member.batch}</h6>
           </div>
         </div>
         `;
-        // let content = `
-        //   <div class="card col-md-8 col-12">
-        //   <div class="card-header" id="heading-${member._id}">
-        //     <h5 class="mb-0">
-        //           ${member.name}
-        //     </h5>
-        //   </div>
-      
-        //   <div id="${member._id}" class="collapse show" >
-        //     <div class="card-body">
-      
-        //       <h5>${member.email}</h5>
-             
-        //       <p>${member.batch}</p>
-        //     </div>
-        //   </div>
-         
-         
-        // </div>
-        // `;
-      
+
               container.innerHTML += content;
+      });
+
+      let prefinal = members.filter(member => member.batch==2022);
+      console.log('prefinal ',prefinal)
+      // document.getElementById("third-year").innerHTML = "";
+      const container1 = document.getElementById("third-year");
+      console.log("third year is ",container1);
+      prefinal.forEach(member => {
+        let url = member.image ?? '/images/user_default.png';
+        if(typeof member.image !== 'string')
+          url = '/images/user_default.png';
+        const card = document.createElement('div');
+        card.classList = 'card-body';
+        // Construct card content
+         content = `
+        <div class='memberCard'>
+          <div style='background-image:url("${member.image}")' class='memberImage'>
+          </div>
+          <div class='memberDetails josephine-sans'>
+            <h5>${member.name}</h5>
+            <h6>${member.batch}</h6>
+          </div>
+        </div>
+        `;
+              container1.innerHTML += content;
       });
     },
     error:function(err){
@@ -72,12 +81,12 @@ function getMemberList(batch)
   })
 }
 
-var menu = document.getElementById("batch");
-  menu.onchange=function(e){
-  e.preventDefault();
+// var menu = document.getElementById("batch");
+//   menu.onchange=function(e){
+//   e.preventDefault();
 
-  getMemberList(menu.value);
-}
+//   getMemberList(menu.value);
+// }
 
 
 // let alumniList = function(alumni){
@@ -96,16 +105,16 @@ var menu = document.getElementById("batch");
 
 
 window.onload = function() {
-  var currentBatch = sessionStorage.getItem("batch");  
-  console.log('batch is ',currentBatch);
-  if(currentBatch==null)
-  {
-    currentBatch = "all";
+  // var currentBatch = sessionStorage.getItem("batch");  
+  // console.log('batch is ',currentBatch);
+  // if(currentBatch==null)
+  // {
+  //   currentBatch = "all";
+  // }
+  // $('#batch').val(currentBatch);
+  getMemberList('all');
   }
-  $('#batch').val(currentBatch);
-  getMemberList(currentBatch);
-  }
-  $('#batch').change(function() { 
-      var selVal = $(this).val();
-      sessionStorage.setItem("batch", selVal);
-  });
+  // $('#batch').change(function() { 
+  //     var selVal = $(this).val();
+  //     sessionStorage.setItem("batch", selVal);
+  // });
